82ed4e2bb1e32c5159ff916e6cb437d5f468ccf3
poikilos <7557867+poikilos@users.noreply.github.com>
2024-08-04 23:44:18 -0400

    Add ProgressWriter.

---
+++ hierosoft/progresswriter.py
@@ -0,0 +1,45 @@
+import sys
+
+class ProgressWriter:
+    cli_print_delta = 1024 * 400  # 1024 * 400 is 400K
+
+    def __init__(self):
+        self.prev_bytes = None
+        self.prev_percent = None
+        self.prev_hr = None  # rounded
+        self.prev_ratio = None  # precise
+        self.hr_fn = None
+
+    def write(self, size, total, force=False):
+        min_d = ProgressWriter.cli_print_delta
+        if self.prev_bytes and (size - self.prev_bytes < min_d):
+            if not force:
+                return
+        percent = None
+        ratio = None
+        if total:
+            ratio = size / total
+            percent = round(size * 100 / total, 1)
+        hr = None
+        if self.hr_fn:
+            hr = self.hr_fn(size)
+        if (self.prev_hr and (hr == self.prev_hr)
+                and (percent is None or percent == self.prev_percent)):
+            if not force:
+                return
+        self.prev_bytes = size
+        self.prev_percent = percent
+        self.prev_ratio = ratio
+
+        if total:
+            # overwrite 4 with spaces ("." and digit, or neither if ".0" x2)
+            sys.stderr.write(
+                "\rprogress={}% size={}    "
+                .format(percent, hr)
+            )
+        else:
+            # overwrite 2 with spaces ("." and digit, or neither if ".0")
+            sys.stderr.write(
+                "\rsize={}  ".format(hr)
+            )
+        sys.stderr.flush()
