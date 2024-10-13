4787086d69af646cdee764bf0dd924e0f40e03ee
poikilos <7557867+poikilos@users.noreply.github.com>
2024-10-13 10:28:13 -0400

    Use latest installed Ardour. Show the error if the wrapped process failed. Implement minimal version splitting (TODO: behave like nopackage, and make nopackage require this?). Add related tests. Update cSpell dict.

---
+++ hierosoft/tests/test_programinfo.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import unittest
+import platform
+import sys
+import os
+
+my_dir = os.path.dirname(os.path.abspath(__file__))
+module_dir = os.path.dirname(my_dir)
+repo_dir = os.path.dirname(module_dir)
+
+if __name__ == "__main__":
+    sys.path.insert(0, repo_dir)
+
+if sys.version_info.major < 3:
+    print("[test_logging] using Python 2 shims for logging.",
+          file=sys.stderr)
+    import hierosoft.morelogging as logging
+else:
+    print("[test_logging] using Python 3 logging.",
+          file=sys.stderr)
+    import logging
+from hierosoft.programinfo import (  # noqa: E402
+    # echo0,
+    # echo1,
+    # echo2,
+    # set_verbosity,
+    split_version,
+    path_split_all,
+    ProgramInfo,
+)
+
+
+
+class TestProgramInfo(unittest.TestCase):
+
+    # def __init__(self):
+    #     if sys.version_info.major >=3:
+    #         super().__init__()
+    #     else:
+    #         # unittest.TestCase.__init__(self)
+    #         # super(TestProgramInfo, self).__init__()
+    #         # Python 2.7.18 says:
+    #         # "ValueError: no such test method in
+    #         # <class '__main__.TestProgramInfo'>: runTest"
+    #         # in either case!
+    #         pass
+
+    def test_path_split_all(self):
+        if platform.system() == "Windows":
+            self.assertEqual(path_split_all("C:\\users\\user"), ["C:", "users", "user"])
+        else:
+            self.assertEqual(path_split_all("/home/user/tmp"), ["/", "home", "user", "tmp"])
+
+    def test_split_version(self):
+        self.assertEqual(split_version("something"), ("something",))
+        self.assertEqual(split_version("something-git"), ("something", "", "git"))
+        self.assertEqual(split_version("something-6.0.0-git"), ("something", "6.0.0", "git"))
+        self.assertEqual(split_version("something-6.0.0-1"), ("something", "6.0.0", "1"))
+        self.assertEqual(split_version("something-6.0.0"), ("something", "6.0.0"))
+
+    def test_program_info_version(self):
+        program = ProgramInfo()
+        self.assertEqual(split_version("Ardour-8.6.0"), ("Ardour", "8.6.0"))
+        self.assertEqual(split_version("ardour8"), ("ardour8",))
+        # ^ otherwise below won't work correctly
+        program.set_version_from_path("/opt/Ardour-8.6.0/bin/ardour8")
+        self.assertEqual(program.name, ("Ardour"))
+        self.assertEqual(program.version, "8.6.0")
+        self.assertEqual(program.version_tuple(), (8, 6, 0))
+        self.assertEqual(program.suffix, None)
+        program = ProgramInfo()
+        program.set_version_from_path("/opt/Ardour/bin/ardour8")
+        self.assertEqual(program.name, "ardour8")
+        self.assertEqual(program.version, None)
+        self.assertEqual(program.version_tuple(), None)
+        self.assertEqual(program.suffix, None)
+        program = ProgramInfo()
+        program.set_version_from_path("/opt/Ardour/bin/ardour-8")
+        self.assertEqual(program.name, "ardour")
+        self.assertEqual(program.version, "8")
+        self.assertEqual(program.version_tuple(), (8,))
+        self.assertEqual(program.suffix, None)
+
+
+if __name__ == "__main__":
+    unittest.main()
+    # testcase = TestProgramInfo()
+    # for name in dir(testcase):
+    #     if name.startswith("test"):
+    #         fn = getattr(testcase, name)
+    #         fn()  # Look at def test_* for the code if tracebacks start here
