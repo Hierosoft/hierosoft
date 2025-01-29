8db8b97b9da445b3df87602ca16b53f4ac60da82
Jacob Gustafson <7557867+Poikilos@users.noreply.github.com>
2025-01-29 15:57:29 -0500

    Add add_newline setting. Add stack_trace to write0.

--- loggingdemo.py
+++ loggingdemo.py
@@ -4,6 +4,8 @@ See logging-demo.py which imports loggingdemo.
 from hierosoft.morelogging import (
     echo0,
     echo1,
+    write0,
+    write1,
 )
 
 
@@ -11,6 +13,10 @@ def foo():
     echo0("Hi")
     echo0()
     echo0("...", stack_trace=False)
+    write0("On one line...")
+    echo0("OK")
+    write1("On one line...")
+    echo1("OK")
     echo1("Bye", multiline=False)
 
 
