64666da2ad8e28abd8eb12c852f02e4d5f370ae3
Jacob Gustafson <7557867+Poikilos@users.noreply.github.com>
2025-11-20 15:50:29 -0500

    Improve docstrings. Assert type in hex_pair.

--- hierosoft/moretk.py
+++ hierosoft/moretk.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+import sys
 # import numpy
 
 
@@ -25,19 +27,22 @@ def rgba_tuple(int_pixel):
     )
 
 
-def hexpair(int_value):
-    """_summary_
+def hex_pair(int_value):
+    """Convert a byte to a hex pair.
 
     Args:
-        int_value (_type_): _description_
+        int_value (int): A number 0-255.
 
     Returns:
-        _type_: _description_
+        str: A string 2 characters long (such as '08' for int_value 8,
+            or 'ff' for int_value 255).
 
     Raises:
-        OverflowError: (Raised by to_bytes since has no length, order
-            params) if "int too big to convert"
+        OverflowError: (Raised by to_bytes since called without length &
+            order params and by default only allows 0-255) if "int too
+            big to convert"
     """
+    assert isinstance(int_value, int)
     value_bytes = int_value.to_bytes()  # if no length param limit is 1 byte
     return value_bytes.hex()
 
@@ -52,10 +57,10 @@ def hex_rgba(int_pixel):
         tuple: r, g, b, a integers
     """
     return (
-        hexpair((int_pixel >> 8) & 0xFF)  # R
-        + hexpair((int_pixel >> 16) & 0xFF)  # G
-        + hexpair(int_pixel >> 24)  # B
-        + hexpair(int_pixel & 0xFF)  # A
+        hex_pair((int_pixel >> 8) & 0xFF)  # R
+        + hex_pair((int_pixel >> 16) & 0xFF)  # G
+        + hex_pair(int_pixel >> 24)  # B
+        + hex_pair(int_pixel & 0xFF)  # A
     )
 
 
@@ -114,3 +119,88 @@ class OffscreenCanvas(object):
                 canvas.create_line(dst_x, dst_y, dst_x+x, dst_y,
                                    fill=hex_color)
                 # ^ +1 since exclusive (draw one pixel in this case)
+
+
+# from tkinter import Frame, Variable, Scrollbar, Text
+# from tkinter.constants import VERTICAL, RIGHT, LEFT, BOTH, END, Y
+
+if sys.version_info.major >= 3:  # imports differ
+    import tkinter as tk
+    from tkinter import ttk
+else:
+    # Python 2
+    import Tkinter as tk  # type:ignore
+    import ttk  # type:ignore
+
+
+class TextExtension(ttk.Frame):
+    """Extends Frame.  Intended as a container for a Text field.
+    Better related data handling and has Y scrollbar.
+
+    [CC BY 4.0](https://creativecommons.org/licenses/by-sa/4.0/)
+    Attribution:
+    answered Oct 22, 2014 at 14:08
+    Nicklas Börjesson
+
+    edited Apr 11, 2023 at 17:23
+    Piti Ongmongkolkul
+
+    edited July 16, 2025
+    by Poikilos (Jake Gustafson)
+    """
+
+    def __init__(self, master, textvariable=None, *args, **kwargs):
+        if sys.version_info.major >= 3:
+            super(TextExtension, self).__init__(master)
+        else:
+            ttk.Frame.__init__(self, master)
+        # Init GUI
+
+        self._y_scrollbar = tk.Scrollbar(self, orient=tk.VERTICAL)
+
+        self._text_widget = tk.Text(self, yscrollcommand=self._y_scrollbar.set,
+                                    *args, **kwargs)
+        self._text_widget.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+
+        self._y_scrollbar.config(command=self._text_widget.yview)
+        self._y_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+        if textvariable is not None:
+            if not (isinstance(textvariable, tk.Variable)):
+                raise TypeError("tkinter.Variable type expected, {} given."
+                                .format(type(textvariable)))
+            self._text_variable = textvariable
+            self.var_modified()
+            self._text_trace = self._text_widget.bind('<<Modified>>',
+                                                      self.text_modified)
+            self._var_trace = textvariable.trace("w", self.var_modified)
+
+    def text_modified(self, *args):
+        if self._text_variable is not None:
+            self._text_variable.trace_vdelete("w", self._var_trace)
+            self._text_variable.set(self._text_widget.get(1.0, 'end-1c'))
+            self._var_trace = self._text_variable.trace("w", self.var_modified)
+            self._text_widget.edit_modified(False)
+
+    def var_modified(self, *args):
+        self.set_text(self._text_variable.get())
+        self._text_widget.edit_modified(False)
+
+    def unhook(self):
+        if self._text_variable is not None:
+            self._text_variable.trace_vdelete("w", self._var_trace)
+
+    def clear(self):
+        self._text_widget.delete(1.0, tk.END)
+
+    def set_text(self, _value):
+        self.clear()
+        if (_value is not None):
+            self._text_widget.insert(tk.END, _value)
+
+    def leaf_cget(self, key):
+        self._text_widget.cget(key)
+
+    def leaf_configure(self, **kwargs):
+        self._text_widget.configure(**kwargs)
+
