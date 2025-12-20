39aacc885a3bb37da83e1fa23f84128d24692e96
Jacob Gustafson <7557867+Poikilos@users.noreply.github.com>
2025-12-20 14:41:46 -0500

    Add missing TCL fix for Windows.

--- run.py
+++ run.py
@@ -1,10 +1,49 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from __future__ import print_function
+
+import os
+import platform
+import re
+import shutil
 import sys
+
+dst_python_scripts = os.path.dirname(sys.executable)
+dst_python_env = os.path.dirname(dst_python_scripts)
+if os.path.isfile(os.path.join(dst_python_env, "pyvenv.cfg")):
+    # Copy TCL to venv to avoid Windows bug
+    # "Can't find a usable init.tcl in the following directories"
+    # (See <https://stackoverflow.com/a/30377257/4541104>)
+    dst_tcl = os.path.join(dst_python_env, "tcl")
+    if ((not os.path.isdir(dst_tcl)) and (sys.version_info.major < 3)
+            and (platform.system() == "Windows")):
+        good_tcl = None
+        for src_python_lib in sys.path:
+            src_python_dir = os.path.dirname(src_python_lib)
+            src_tcl = os.path.join(src_python_dir, "tcl")
+            if os.path.isdir(src_tcl):
+                good_tcl = src_tcl
+                sys.stderr.write("cp -R {} {} # ...".format(repr(src_tcl), repr(dst_tcl)))
+                sys.stderr.flush()
+                shutil.copytree(src_tcl, dst_tcl)
+                if os.path.splitext(os.path.split(sys.executable)[1])[0].lower() == "python":
+                    # Using python
+                    run_cmd = [sys.executable] + sys.argv
+                else:
+                    # Using precompiled Python program (Needs testing)
+                    run_cmd = sys.argv
+                print("OK. Relaunching {}...".format(run_cmd), file=sys.stderr)
+                import subprocess
+                subprocess.Popen(run_cmd)
+                sys.exit(0)
+                break
+        if not good_tcl:
+            print("[run] Warning: no tcl in {}".format(sys.path), file=sys.stderr)
+    else:
+        print("[run] Using {}".format(dst_tcl))
+
 from hierosoft.gui_tk import main as gui_main
 from hierosoft.moreweb.hierosoftupdate import main as update_main
-import re
-
 
 def main():
     prefix = "[main] "
